1853689c7dd9bcaf1a4380e2eb42650627c0accf
RRizel <royrizel@gmail.com>
2025-05-25 19:56:35 +0300

    Add www and server.js with server and app initialization

--- DataBase/server.js
+++ DataBase/server.js
@@ -1,19 +1,10 @@
-const mongoose = require('mongoose');
+/**
+ * This file exports the Express app instance.
+ * The app should be imported here to be used in server startup scripts.
+ *
+ * @module server
+ */
+
 const app = require('../app');
 
-//const uri = 'mongodb+srv://stavsi:stav102102@expensetracker-nodejspr.ztpnj0f.mongodb.net/ExpenseTracker?retryWrites=true&w=majority&appName=ExpenseTracker-nodejsProject';
-const uri = process.env.MONGO_URI;
-
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('MongoDB connected');
-
-    app.listen(3000, () => {
-        console.log('Server running on port 3000');
-    });
-
-}).catch(err => {
-    console.error('MongoDB connection error:', err);
-});
+module.exports = app;
